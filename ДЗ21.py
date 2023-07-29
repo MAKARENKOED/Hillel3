b526b146c96e30097147b5ca62c80de74a782a5f
MAKARENKOED <100123765+MAKARENKOED@users.noreply.github.com>
2023-07-29 15:57:17 +0200

    Update ДЗ21.py

--- ДЗ21.py
+++ ДЗ21.py
@@ -9,9 +9,7 @@ dict_1 = {
     678901: ('Даниил', 31)
 }
 
-# Переформатируем словарь для сохранения в json, так как json не поддерживает int в качестве ключа
 dict_1 = {str(key): value for key, value in dict_1.items()}
 
-# Запись словаря в json-файл
 with open('dictionary.json', 'w') as file:
     json.dump(dict_1, file)
